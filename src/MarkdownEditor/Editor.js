47438d282a7bd05db0ab5acf736f99a2bc39748f
MaGaroo <amoomajid99@gmail.com>
2019-03-26 16:16:39 +0430

    Remove unnecessary attributes of md editor form

--- src/MarkdownEditor/Editor.js
+++ src/MarkdownEditor/Editor.js
@@ -18,7 +18,7 @@ export class Editor extends React.Component {
 
   render() {
     return (
-      <Form method="post" action="http://localhost:8080/lessons/add">
+      <Form>
         <Input type="text" name="title" className="Title" value={this.props.title} onChange={ this.props.titleChange } placeholder="عنوان درس" />
         <Input type="textarea" name="text" className="Editor dark" value={this.props.text} onChange={ this.props.textChange } placeholder="متن درس" />
         <Button color="primary" className="Send-button" onClick={ this.onClick.bind(this) }>ارسال</Button>
